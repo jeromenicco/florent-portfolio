133fe1545cbb5b75df59770d1ba1ca9d4dcb434c
Romain Caussignac <romaincaussignac@macbook-pro-de-romain.home>
2021-10-12 16:28:13 +0200

    test width marquee

--- src/components/TickerText.js
+++ src/components/TickerText.js
@@ -1,38 +1,23 @@
 import React from "react"
 
+// import { usePageVisible } from "../effects/usePageVisible"
+
 import Ticker from "react-ticker"
 
 import "./TickerText.css"
 
 function TickerText() {
-  // const [viewWidth, setViewWidth] = useState(Number)
-  // console.log("outside useEffect", viewWidth)
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setViewWidth(window.innerWidth)
-  //   }
-  //   window.addEventListener("resize", handleResize)
-  //   return () => {
-  //     setViewWidth(window.innerWidth)
-  //   }
-  // }, [viewWidth])
-
-  // const tickerRef = useRef(null)
-  // useEffect(() => {
-  //   console.log("tickerREF width", tickerRef.current.offsetWidth);
-  // }, []);
-
+  // const isPageVisible = usePageVisible();
   return (
     <div className="ticker__container">
       <Ticker>
         {({index}) => (
           //({}) => ... to pass props
-            <p className="ticker__text">
-              Hi! I am Florent, art director, motion designer and visual artist. 
-              Salut! Je suis Florent, directeur artistique, motion designer et
-              artiste visuel.
-            </p>
+            <>
+            <p className='ticker__text'>
+            Hi! I am Florent, art director, motion designer and visual artist. Salut! Je suis Florent, directeur artistique, motion designer et artiste visuel.
+          </p>
+          </>
         )}
       </Ticker>
     </div>
