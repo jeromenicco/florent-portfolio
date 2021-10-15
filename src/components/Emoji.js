31903f1793cd2d136903bd53a369b093d0f150e5
Romain Caussignac <romaincaussignac@macbook-pro-de-romain.home>
2021-10-15 15:02:17 +0200

    random duration emoji ok

---
+++ src/components/Emoji.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState} from 'react'
+
+import { useSpring, animated } from "react-spring"
+
+const Emoji = ({src}) => {
+    const [random, setRandom] = useState(Number)
+    const screenWidth = window.innerWidth
+
+    const shuffle = () => {
+        const number = Math.random() * (3000 - 10000) + 10000
+        setRandom(number)
+    } 
+
+    const emojiSpring = useSpring({
+        loop: () => {
+            shuffle()
+            return true
+        },
+        from: {
+            transform: `translate(-100%)`,
+        },
+        to: {
+            transform: `translate(${screenWidth}px)`,
+        },
+        config: {
+            duration: random,
+        },
+    })
+
+    console.log(emojiSpring)
+
+    return (
+        <animated.div
+            style={{
+              ...emojiSpring,
+            }}
+            className="emoji__rail"
+        >
+            <img src={src} alt="unicorn-emoji" />
+      </animated.div>
+    )
+}
+
+export default Emoji
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,6 +4,8 @@ import { useSpring, animated } from "react-spring"
 
 import TickerText from "./TickerText"
 
+import Emoji from './Emoji'
+
 import heartEMOJI from "../assets/gifs/EMOJI_COEUR.gif"
 import faceEMOJI from "../assets/gifs/EMOJI_FACE.gif"
 import unicornEMOJI from "../assets/gifs/EMOJI_LICORNE.gif"
@@ -12,103 +14,14 @@ import bearEMOJI from "../assets/gifs/EMOJI_OURS.gif"
 import "./TopBar.css"
 
 function TopBar() {
-  const screenWidth = window.innerWidth
-
-  const emojiSpring = useSpring({
-    loop: true,
-    from: {
-      transform: `translate(-100%)`,
-    },
-    to: {
-      transform: `translate(${screenWidth}px)`,
-    },
-    config: {
-      duration: Math.random() * (9000 - 4000) + 4000,
-    },
-  })
-
-  console.log(emojiSpring)
-
-  const emojiSpring2 = useSpring({
-    loop: true,
-    from: {
-      transform: `translate(-${screenWidth}px)`,
-    },
-    to: {
-      transform: `translate(${screenWidth}px)`,
-    },
-    config: {
-      duration: screenWidth * 6,
-      // duration: 8000,
-      delay: 5000,
-    },
-  })
-
-  const emojiSpring3 = useSpring({
-    loop: true,
-    from: {
-      transform: `translate(-${screenWidth + screenWidth}px)`,
-    },
-    to: {
-      transform: `translate(${screenWidth}px)`,
-    },
-    config: {
-      duration: screenWidth * 9.3,
-      delay: 2000,
-    },
-  })
-
-  const emojiSpring4 = useSpring({
-    loop: true,
-    from: {
-      transform: `translate(-${screenWidth * 1.5}px)`,
-    },
-    to: {
-      transform: `translate(${screenWidth}px)`,
-    },
-    config: {
-      duration: screenWidth * 5.2,
-    },
-  })
 
   return (
     <div className="top__bar__container">
-      <animated.div
-        style={{
-          ...emojiSpring,
-        }}
-        className="emoji__rail"
-      >
-        <img src={unicornEMOJI} alt="unicorn-emoji" />
-      </animated.div>
-
-      <animated.div
-        style={{
-          ...emojiSpring2,
-        }}
-        className="emoji__rail"
-      >
-        <img src={heartEMOJI} alt="heart-emoji" />
-      </animated.div>
-
-      <animated.div
-        style={{
-          ...emojiSpring3,
-        }}
-        className="emoji__rail"
-      >
-        <img src={faceEMOJI} alt="face-emoji" />
-      </animated.div>
-
-      <animated.div
-        style={{
-          ...emojiSpring4,
-        }}
-        className="emoji__rail"
-      >
-        <img src={bearEMOJI} alt="bear-emoji" />
-      </animated.div>
-        <TickerText />
+      <Emoji src={unicornEMOJI} />
+      <Emoji src={faceEMOJI} />
+      <Emoji src={heartEMOJI} />
+      <Emoji src={bearEMOJI} />
+      <TickerText />
     </div>
   )
 }
