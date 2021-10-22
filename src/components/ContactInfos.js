2e77fffeddd6b5d7707608e97ef4a13f426acbee
Romain Caussignac <romaincaussignac@macbook-pro-de-romain.home>
2021-10-22 14:56:13 +0200

    mobile version, titles bold test

--- src/components/ContactInfos.js
+++ src/components/ContactInfos.js
@@ -15,6 +15,7 @@ function ContactInfos() {
         Contactez-moi à l'adresse
         <a className='infos__mail__link' href='mailto:florent.texier@protonmail.com'> florent.texier@protonmail</a> !
       </p>
+      <div className='dum__space__contact' />
       <p className="infos__content1">
       Since 2013, I work for medias, publishing houses, institutions and brands. My experimentations focus on 2D and 3D animation, film direction, illustration and printed objects.
       </p>
