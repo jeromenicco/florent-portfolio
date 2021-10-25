05edc8f3ab175877480ed077dab8971ac74571d1
Romain Caussignac <romaincaussignac@macbook-pro-de-romain.home>
2021-10-25 11:13:53 +0200

    test

--- src/components/ContactInfos.js
+++ src/components/ContactInfos.js
@@ -7,9 +7,7 @@ function ContactInfos() {
   return (
     <FadeIn className="infos__container">
       <p className="infos__content1">
-        Depuis 2013, je suis directeur artistique, designer graphique et motion
-        designer pour des acteurs institutionnels, culturels, des médias et des
-        marques.<br></br>
+      Depuis 2013, je travaille pour des médias, des acteurs culturels, des maisons d’édition et des marques. J’expérimente aussi dans les champs de l’animation 2D et 3D, la réalisation, l’illustration et les objets imprimés. <br></br>
         Contactez-moi à l'adresse <a className='infos__mail__link' href='mailto:florent.texier@protonmail.com'> florent.texier@protonmail</a> !
       </p>
       <div className='dum__space__contact' />
