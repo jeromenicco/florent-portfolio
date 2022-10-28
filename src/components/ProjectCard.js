dea772d67f64c2e0020b57fbf4c383e94514a599
Jerome Nicco <jeromenicco36@gmail.com>
2022-10-28 17:32:53 +0200

    iframe deploy v1

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import FadeIn from "react-fade-in"
 import { Swiper, SwiperSlide } from "swiper/react"
-import SwiperCore, { EffectFade, Pagination, Navigation} from "swiper"
+import SwiperCore, { EffectFade, Pagination, Navigation } from "swiper"
 import { useDispatch } from "react-redux"
 import { setVisible, setProject } from "../redux/slices/fullScreenSlice"
 import { useMediaQuery } from "react-responsive"
+
+import ReactPlayer from 'react-player'
+
 
 import "swiper/swiper-bundle.css"
 import "./ProjectCard.css"
@@ -15,61 +18,83 @@ function ProjectCard({ item }) {
 
   const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" })
 
+
   const handleFullScreen = () => {
     dispatch(setProject(item))
     dispatch(setVisible(true))
   }
 
+
+ 
+
   return (
     <FadeIn className="card__container">
-        <Swiper
-          className="swiper__container"
-          slidesPerView={1}
-          effect={'fade'}
-          spaceBetween={10}
-          loop={true}
-          pagination
-          navigation={isDesktop ? true : false}
-          allowTouchMove={isDesktop ? false : true}
-        >
-          {
-            item.img.map((item, index) => (
+      <Swiper
+        className="swiper__container"
+        slidesPerView={1}
+        effect={'fade'}
+        spaceBetween={10}
+        loop={true}
+        pagination
+        navigation={item.img.length > 1 && isDesktop ? true : false}
+        allowTouchMove={isDesktop ? false : true}
+      >
+        {
+          item.img.map((item, index) => (
+            !item.includes(".jpg")
+              ?
+              <div
+                className='iframe__wrapper'
+                style={{
+                  position: "relative",
+                  paddingBottom: "56.25%" /* 16:9 */,
+                  paddingTop: 25,
+                  marginBottom: isDesktop && '5%',
+                  height: 0
+                }}
+              >
+                <iframe
+                  id='i_frame'
+                  key={index}
+                  className="iframe"
+                  title={item}
+                  src={`http://www.youtube.com/embed/${item}?autoplay=1&mute=1&loop=1&playlist=${item}`}
+                  frameBorder='0'
+                  allow='autoplay; encrypted-media'
+                  allowFullScreen
+                  width={'100%'}
+                  // height={isDesktop ? 500 : 250}
+                  controls
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+              </div>
+              :
               <SwiperSlide key={index} onClick={isDesktop ? handleFullScreen : null}>
-                  {
-                    item.includes(".mp4")
-                    ?
-                    <video
-                      className="project__img"
-                      autoPlay
-                      muted
-                      loop
-                      playsInline
-                      alt={item.title}
-                    >
-                      <source src={item} type="video/mp4" />
-                    </video>
-                    :
-                    <div>
-                      <img className="project__img" src={item} alt={item.title} />
-                    </div>
-                  }
-                </SwiperSlide>
-            ))
-          }
-        </Swiper>
-        <div className="card__text__container">
-          <div className="title__arrow__container">
-            <p className="card__title" onClick={isDesktop ? handleFullScreen : null}>{item.title}</p>
-          </div>
-          <div>
-            <p>{ item.resume && item.resume} <span style={{ fontStyle: "italic"}}>{item.resumeItalic && item.resumeItalic}</span></p>
-            {
-              item.link && 
-              <a target="_blank" rel="noreferrer" href={item.url}>{item.link}</a>
-            }
-          </div>
+                {
+                  <img className="project__img" src={item} alt={item.title} />
+                }
+              </SwiperSlide>
+          ))
+        }
+      </Swiper>
+      <div
+        className="card__text__container"
+      >
+        <div className="title__arrow__container">
+          <p className="card__title" onClick={isDesktop ? handleFullScreen : null}>{item.title}</p>
         </div>
-      </FadeIn>
+        <div>
+          <p><span style={{ fontStyle: "italic" }}>{item.resumeItalic && item.resumeItalic}</span> {item.resume && item.resume}</p>
+          {item.link && <a target="_blank" rel="noreferrer" href={item.url}>{item.link}</a>}
+        </div>
+      </div>
+    </FadeIn>
   )
 }
 
--- src/data.js
+++ src/data.js
@@ -1,3 +1,14 @@
+import CM1 from "./assets/images/CM/CM1.jpg"
+import CM2 from "./assets/images/CM/CM2.jpg"
+import CM3 from "./assets/images/CM/CM3.jpg"
+import CM4 from "./assets/images/CM/CM4.jpg"
+import CM5 from "./assets/images/CM/CM5.jpg"
+
+import VEUILLEZ1 from "./assets/images/VEUILLEZ/VEUILLEZ1.jpg"
+import VEUILLEZ2 from "./assets/images/VEUILLEZ/VEUILLEZ2.jpg"
+import VEUILLEZ3 from "./assets/images/VEUILLEZ/VEUILLEZ3.jpg"
+import VEUILLEZ4 from "./assets/images/VEUILLEZ/VEUILLEZ4.jpg"
+
 import UR1 from "./assets/images/U&R/U&R.jpg"
 import UR2 from "./assets/images/U&R/U&R2.jpg"
 import UR3 from "./assets/images/U&R/U&R3.jpg"
@@ -6,11 +17,10 @@ import UR5 from "./assets/images/U&R/U&R5.jpg"
 import UR6 from "./assets/images/U&R/U&R6.jpg"
 import UR7 from "./assets/images/U&R/U&R7.jpg"
 
-import DD1 from "./assets/images/DATADRAMA/DATADRAMA.jpg"
-import DD2 from "./assets/images/DATADRAMA/DATADRAMA2.jpg"
-import DD3 from "./assets/images/DATADRAMA/DATADRAMA3.jpg"
-import DD4 from "./assets/images/DATADRAMA/DATADRAMA4.jpg"
-import DD5 from "./assets/images/DATADRAMA/DATADRAMA5.jpg"
+import DD1 from "./assets/images/DATADRAMA/DATADRAMA_1.jpg"
+import DD2 from "./assets/images/DATADRAMA/DATADRAMA_2.jpg"
+import DD3 from "./assets/images/DATADRAMA/DATADRAMA_3.jpg"
+import DD4 from "./assets/images/DATADRAMA/DATADRAMA_4.jpg"
 
 import FIF1 from "./assets/images/FIF/FIF85.jpg"
 import FIF2 from "./assets/images/FIF/FIF852.jpg"
@@ -23,17 +33,17 @@ import LAC2 from "./assets/images/LAC/LAC2.jpg"
 import LAC3 from "./assets/images/LAC/LAC3.jpg"
 import LAC4 from "./assets/images/LAC/LAC4.jpg"
 
-import TFC1 from "./assets/images/TFC/TFC1_GENERIQUE_1.mp4"
+// import TFC1 from "./assets/images/TFC/TFC1_GENERIQUE_1.mp4"
 import TFC2 from "./assets/images/TFC/TFC2.jpg"
-import TFC3 from "./assets/images/TFC/TFC3_BIRTH_1.mp4"
+// import TFC3 from "./assets/images/TFC/TFC3_BIRTH_1.mp4"
 import TFC4 from "./assets/images/TFC/TFC4.jpg"
-import TFC5 from "./assets/images/TFC/TFC5_ROCKET_1.mp4"
+// import TFC5 from "./assets/images/TFC/TFC5_ROCKET_1.mp4"
 import TFC6 from "./assets/images/TFC/TFC6.jpg"
 
 import KONBINI01 from "./assets/images/KONBINI/KONBINI01.jpg"
-import KONBINI02 from "./assets/images/KONBINI/KONBINI02_1.mp4"
-import KONBINI03 from "./assets/images/KONBINI/KONBINI03_1.mp4"
-import KONBINI04 from "./assets/images/KONBINI/KONBINI04_1.mp4"
+// import KONBINI02 from "./assets/images/KONBINI/KONBINI02_1.mp4"
+// import KONBINI03 from "./assets/images/KONBINI/KONBINI03_1.mp4"
+// import KONBINI04 from "./assets/images/KONBINI/KONBINI04_1.mp4"
 
 import RICHEMONT from "./assets/images/RICHEMONT/RICHEMONT.jpg"
 import RICHEMONT2 from "./assets/images/RICHEMONT/RICHEMONT2.jpg"
@@ -59,69 +69,114 @@ import TRIBAM3 from "./assets/images/TRIBAM/TRIBAM3.jpg"
 
 export const projectList = [
   {
+    title: "Champ Magnétique",
+    resume: "Premier numéro des Cahiers de La Maison Forte, un tiers-lieu en Lot-et-Garonne dédié aux cultures de la transition.",
+    resumeItalic: "~ Direction artistique, édition ~",
+    link: "",
+    img: [CM1, CM2, CM3, CM4, CM5],
+  },
+  {
+    title: "Veuillez libérer de l’espace de stockage",
+    resume: "Micro-édition nocturne, érotique et poétique qui plonge dans l’intimité des images stockées sur nos smartphones.",
+    resumeItalic: "~ Illustration, édition ~",
+    link: "",
+    img: [VEUILLEZ1, VEUILLEZ2, VEUILLEZ3, VEUILLEZ4],
+  },
+  {
+    title: "Interview • Stand-up",
+    resume: "Deux formats vidéos de L’ADN, destinés aux réseaux sociaux.",
+    resumeItalic: "~ Identité visuelle, motion design ~",
+    link: "L’ADN sur Instagram.",
+    url: "https://www.instagram.com/ladneu/reels/",
+    img: ['Zb7fvCTFf6A'],
+  },
+  {
+    title: "L’actu en bref • Idée forte",
+    resume: "Deux formats vidéos de L’ADN, destinés aux réseaux sociaux.",
+    resumeItalic: "~ Identité visuelle, motion design ~",
+    link: "L’ADN sur Instagram.",
+    url: "https://www.instagram.com/ladneu/reels/",
+    img: ['7xgFWtjtKCQ'],
+  },
+  {
     title: "Usbek & Rica magazine",
-    resume: "Le magazine qui explore le futur, trimestriellement.",
-    resumeItalic: "Direction artistique, design éditorial.",
+    resume: "Le magazine qui explore le futur. Publication trimestrielle.",
+    resumeItalic: "~ Direction artistique, édition ~",
     img: [UR1, UR2, UR3, UR4, UR5, UR6, UR7],
   },
   {
     title: "DATADRAMA",
-    resume: "Roman graphique de science-fiction sur la technologie et la pornographie. Projet en cours.",
-    resumeItalic: "Écriture, illustrations digitales, expérimentations graphiques.",
-    img: [DD1, DD2, DD3, DD4, DD5],
+    resume: "16 pages micro-éditées rassemblant des images et textes de science-fiction technologique et pornographiques.",
+    resumeItalic: "~ Peinture digitale, édition expérimentale ~",
+    img: [DD1, DD2, DD3, DD4],
   },
   {
     title: "F.I.F.85",
-    resumeItalic: "Direction artistique, typographie.",
+    resume: "Proposition d’identité pour le festival yonnais dédié au cinéma international.",
+    resumeItalic: "~ Identité visuelle, typographie ~",
     img: [FIF1, FIF2, FIF3, FIF4, FIF5],
   },
   {
     title: "The Final Countdown",
-    resume: "Talk-show en ligne d’ArianeGroup au sujet de l’actualité spatiale.",
-    resumeItalic: "Direction artistique, habillage graphique, motion design.",
+    resume: "Talk-show d’ArianeGroup au sujet de l’actualité spatiale.",
+    resumeItalic: "~ Identité visuelle, habillage graphique, motion design ~",
     link: "Voir l'émission.",
     url: "https://www.facebook.com/watch/live/?v=539265316926153&ref=watch_permalink",
-    img: [TFC1, TFC2, TFC3, TFC4, TFC5, TFC6],
+    img: [
+      'WQ5L6ANQBuI'
+      // TFC1,
+      // TFC2,
+      // TFC3,
+      // TFC4,
+      // TFC5,
+      // TFC6
+    ],
   },
   {
-    title: "L’autre Canal — Nancy",
-    resume: "Programmes trimestriels des concerts de musiques actuelles.",
-    resumeItalic: "Direction artistique, design éditorial, illustration.",
+    title: "L’autre Canal",
+    resume: "Programmes trimestriels des concerts de la salle nancéienne de musiques actuelles.",
+    resumeItalic: "~ Direction artistique, édition, illustration ~",
     img: [LAC1, LAC2, LAC3, LAC4],
   },
   {
     title: "Podcasts",
-    resume: "Les 5 émissions d’Usbek & Rica.",
-    resumeItalic: "Direction artistique, réseaux sociaux.",
+    resume: "5 émissions sur le futur par Usbek & Rica.",
+    resumeItalic: "~ Identité visuelle ~",
     img: [PODCASTS1, PODCASTS2, PODCASTS3, PODCASTS4],
   },
   {
     title: "Josie’s Past",
-    resume: "Clip pour le morceau « Josie’s Past » du groupe Xiu Xiu, inspiré du journal secret de Laura Palmer.",
-    resumeItalic: "Réalisation, montage, motion design.",
+    resume: "Vidéo musicale pour le morceau « Josie’s Past » du groupe Xiu Xiu, inspiré du journal secret de Laura Palmer.",
+    resumeItalic: "~ Réalisation, montage, motion design ~",
     link: "Voir la vidéo.",
     url: "https://www.youtube.com/watch?v=8A4FxKXL5pk&t=205s",
     img: [JOSIE1, JOSIE2, JOSIE3, JOSIE4, JOSIE5],
   },
   {
-    title: "Trinitaires / BAM — Metz",
-    resume: "Programmes communs pour les concerts des 2 salles de musiques actuelles.",
-    resumeItalic: "Direction artistique, typographie, design éditorial (proposition).",
+    title: "Trinitaires / BAM",
+    resume: "Programmes communs (propositions) pour les concerts des 2 salles de musiques actuelles de Metz.",
+    resumeItalic: "~ Identité visuelle, typographie, édition ~",
     img: [TRIBAM, TRIBAM2, TRIBAM3],
   },
   
   {
-    title: "Richemont",
-    resume: "Étude sur le futur du tailoring et du costume.",
-    resumeItalic: "Direction artistique, design éditorial.",
+    title: "Le costume a son avenir derrière lui",
+    resume: "Étude sur le futur du tailoring et du costume, pour Richemont.",
+    resumeItalic: "~ Direction artistique, édition ~",
     img: [RICHEMONT, RICHEMONT2, RICHEMONT3, RICHEMONT4, RICHEMONT5],
   },
   {
-    title: "Konbini x N26",
-    resume: "Vidéo explicative retraçant l’histoire des néobanques.",
-    resumeItalic: "Montage, motion design.",
+    title: "Les néobanques",
+    resume: "Vidéo explicative (explainer) retraçant l’histoire des néobanques, pour Konbini x N26.",
+    resumeItalic: "~ Montage, motion design ~",
     link: "Voir la vidéo.",
     url: "https://www.facebook.com/konbinifr/videos/512529209883788/?app=fbl",
-    img: [KONBINI01, KONBINI02, KONBINI03, KONBINI04],
+    img: [
+      // KONBINI01,
+      'oeAAKnn0GPw'
+      // KONBINI02,
+      // KONBINI03,
+      // KONBINI04
+    ],
   },
 ]
